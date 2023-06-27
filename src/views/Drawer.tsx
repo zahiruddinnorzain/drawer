4de83860a60a3d248088b87d8d1d3660b46ef7e0
zahiruddin <kepaknaga@gmail.com>
2023-06-27 16:53:24 +0800

    Update Drawer.tsx

--- src/views/Drawer.tsx
+++ src/views/Drawer.tsx
@@ -548,7 +548,7 @@ export default class Drawer extends React.PureComponent<Props> {
                   style={[
                     styles.overlay,
                     {
-                      opacity: interpolate(this.progress, {
+                      opacity: interpolateNode(this.progress, {
                         inputRange: [PROGRESS_EPSILON, 1],
                         outputRange: [0, 1],
                       }),
